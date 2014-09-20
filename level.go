49f0b030ddf23fdb5cc7989483828045c1eb2f64
George Jiglau <george@mux.ro>
2014-09-20 21:48:44 +0300

    The Level methods now received a pointer

--- level.go
+++ level.go
@@ -20,83 +20,83 @@ func (l *Level) log(lvl, s string) {
 	})
 }
 
-func (l Level) Debug(v ...interface{}) {
+func (l *Level) Debug(v ...interface{}) {
 	l.log("debug", fmt.Sprint(v...))
 }
 
-func (l Level) Debugf(format string, v ...interface{}) {
+func (l *Level) Debugf(format string, v ...interface{}) {
 	l.log("debug", fmt.Sprintf(format, v...))
 }
 
-func (l Level) Debugln(v ...interface{}) {
+func (l *Level) Debugln(v ...interface{}) {
 	l.log("debug", fmt.Sprint(v...))
 }
 
-func (l Level) Info(v ...interface{}) {
+func (l *Level) Info(v ...interface{}) {
 	l.log("info", fmt.Sprint(v...))
 }
 
-func (l Level) Infof(format string, v ...interface{}) {
+func (l *Level) Infof(format string, v ...interface{}) {
 	l.log("info", fmt.Sprintf(format, v...))
 }
 
-func (l Level) Infoln(v ...interface{}) {
+func (l *Level) Infoln(v ...interface{}) {
 	l.log("info", fmt.Sprint(v...))
 }
 
-func (l Level) Warn(v ...interface{}) {
+func (l *Level) Warn(v ...interface{}) {
 	l.log("warn", fmt.Sprint(v...))
 }
 
-func (l Level) Warnf(format string, v ...interface{}) {
+func (l *Level) Warnf(format string, v ...interface{}) {
 	l.log("warn", fmt.Sprintf(format, v...))
 }
 
-func (l Level) Warnln(v ...interface{}) {
+func (l *Level) Warnln(v ...interface{}) {
 	l.log("warn", fmt.Sprint(v...))
 }
 
-func (l Level) Error(v ...interface{}) {
+func (l *Level) Error(v ...interface{}) {
 	l.log("error", fmt.Sprint(v...))
 }
 
-func (l Level) Errorf(format string, v ...interface{}) {
+func (l *Level) Errorf(format string, v ...interface{}) {
 	l.log("error", fmt.Sprintf(format, v...))
 }
 
-func (l Level) Errorln(v ...interface{}) {
+func (l *Level) Errorln(v ...interface{}) {
 	l.log("error", fmt.Sprint(v...))
 }
 
-func (l Level) Panic(v ...interface{}) {
+func (l *Level) Panic(v ...interface{}) {
 	s := fmt.Sprint(v...)
 	l.log("panic", s)
 	panic(s)
 }
 
-func (l Level) Panicf(format string, v ...interface{}) {
+func (l *Level) Panicf(format string, v ...interface{}) {
 	s := fmt.Sprintf(format, v...)
 	l.log("panic", s)
 	panic(s)
 }
 
-func (l Level) Panicln(v ...interface{}) {
+func (l *Level) Panicln(v ...interface{}) {
 	s := fmt.Sprint(v...)
 	l.log("panic", s)
 	panic(s)
 }
 
-func (l Level) Fatal(v ...interface{}) {
+func (l *Level) Fatal(v ...interface{}) {
 	l.log("fatal", fmt.Sprint(v...))
 	os.Exit(1)
 }
 
-func (l Level) Fatalf(format string, v ...interface{}) {
+func (l *Level) Fatalf(format string, v ...interface{}) {
 	l.log("fatal", fmt.Sprintf(format, v...))
 	os.Exit(1)
 }
 
-func (l Level) Fatalln(v ...interface{}) {
+func (l *Level) Fatalln(v ...interface{}) {
 	l.log("fatal", fmt.Sprint(v...))
 	os.Exit(1)
 }
